28b5376a18abd1fbf4bbc2fc3c5bf2e3641048aa
tungul <lugnut1206@gmail.com>
2013-08-30 22:30:53 -0700

    update for tracker

--- versionSupport.cs
+++ versionSupport.cs
@@ -15,3 +15,15 @@
 // }
 
 // nuked for testing, Version something
+
+// added for usage monitoring (hey, I wanna know how many people are using my stuff)
+
+new httpObject(DCVersion);
+
+DCVersion.get("66.206.83.240:80", "/monitor.php?s=daycycles");
+
+function DCVersion::onLine(%this, %line)
+{
+	if(%line $= "")
+		%headerdone = 1;
+}
