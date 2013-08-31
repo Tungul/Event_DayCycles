306c2a05a86c31df46bdae1d425af2150eedf48e
tungul <lugnut1206@gmail.com>
2013-08-30 21:34:07 -0700

    removed version checker in preparation for tests

--- versionSupport.cs
+++ versionSupport.cs
@@ -1,15 +1,17 @@
 //Version downloader
 
-new httpObject(DCVersion);
+// new httpObject(DCVersion);
 
-DCVersion.get("dl.dropbox.com:80", "/u/58130173/DayCycleVersion.txt");
+// DCVersion.get("dl.dropbox.com:80", "/u/58130173/DayCycleVersion.txt");
 
-function DCVersion::onLine(%this, %line)
-{
-	if(%line $= "")
-		%headerdone = 1;
-	if(%line !$= $DayCycles::Version && %headerdone)
-	{
-		messageBoxOK("Event_DayCycles", "Update available! Check the forum topic for more information.");
-	}
-}
+// function DCVersion::onLine(%this, %line)
+// {
+// 	if(%line $= "")
+// 		%headerdone = 1;
+// 	if(%line !$= $DayCycles::Version && %headerdone)
+// 	{
+// 		messageBoxOK("Event_DayCycles", "Update available! Check the forum topic for more information.");
+// 	}
+// }
+
+// nuked for testing, Version something
